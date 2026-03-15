31ce9ea7ff72854786fa1d6c3847cc5a53fcde8d
Shivam Kumar <roysinghaniashivam@gmail.com>
2026-03-15 11:49:13 +0530

    feat: setup database connection prepare backend for storing users

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,20 @@
 import express from "express";
+import dotenv from "dotenv";
 
+import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5001;
 
 const app = express();
 
+app.use(express.json());
+
 app.use("/api/auth", authRoutes);
 
-app.listen(5001, () => console.log("Server running on port 5001"));
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server running on port ${PORT}`);
+});
