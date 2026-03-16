117e0e8421fe82c4090ec106249ccb6c62c41e83
Shivam Kumar <roysinghaniashivam@gmail.com>
2026-03-16 15:25:52 +0530

    feat: add protected authentication routes implement update-profile and check-auth endpoints using auth middleware integrate Cloudinary setup for profile image uploads

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
@@ -11,6 +12,8 @@ const PORT = process.env.PORT || 5001;
 const app = express();
 
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
